1e036d9a6f8bafce524dc11c72382d69b44a7f86
Baseer Ahmed <102865390+BaseerAhmedTunio007@users.noreply.github.com>
2024-02-28 12:34:54 +0500

    Update variables.js

--- first_lecture/variables.js
+++ first_lecture/variables.js
@@ -2,7 +2,7 @@ const  accountID = 54;
 let accountName = "baseer"
 var accountEmail = "baseertunio4@gmail.com";
 
-accountName = "baseer Ahmed";
-accountEmail = "arshad4@gmail.com";
+  accountName = "baseer Ahmed";
+  accountEmail = "arshad4@gmail.com";
 
-console.table([accountID  , accountName , accountEmail]);
+   console.table([accountID  , accountName , accountEmail]);
