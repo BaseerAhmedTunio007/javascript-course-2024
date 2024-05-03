38dd4768db1d8b07f8af3e7f2537b4b1969abe65
Baseer Ahmed <102865390+BaseerAhmedTunio007@users.noreply.github.com>
2024-05-03 12:32:21 +0000

    container config done

---
+++ first_lecture/nums_and _maths.js
@@ -0,0 +1,52 @@
+const balance = new Number(400.00);
+const othernumber = 333.898
+
+ console.log(balance);
+
+ console.log(balance.toString())
+
+ console.log(balance.toString().length)
+
+  //  decimal number use and show fiexed 
+
+ console.log(balance.toFixed(2))
+
+
+//  round of number use the toprecision
+
+console.log(othernumber.toPrecision(4))
+
+
+
+
+//  assignment number prototype use are check 
+// to localstring are use currency use the another counntry
+
+
+//+++++++++++Math ++++++++++++++/////
+
+
+console.log(Math);
+
+// assigment Math function use are and another types and parts function 
+
+// auto change the postive value the function are used in abs 
+console.log(Math.abs(-4)) 
+//  round the numbers 
+console.log(Math.round(6.1))
+
+console.log(Math.ceil(6.2))
+
+console.log(Math.floor(4.3))
+
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random()*(max - min +1) + min ))
+
+
+
+
+
+
