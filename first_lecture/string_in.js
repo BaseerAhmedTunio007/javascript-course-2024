20599130f551f49ae3f67326656d39ce3ebe19ec
Baseer Ahmed <102865390+BaseerAhmedTunio007@users.noreply.github.com>
2024-05-03 12:30:15 +0000

    container config done

---
+++ first_lecture/string_in.js
@@ -0,0 +1,37 @@
+const name = "this is ali";
+const repoCount = 50;
+  console.log(`hello ny name is ${name} and my repo count is ${repoCount}`);
+
+  const gameName = new String("Baseer");
+
+
+    //  check the  length postion   
+  console.log(gameName.length);
+    //  channge the uperCase 
+  console.log(gameName.toUpperCase());
+
+  // check the charector position 
+  console.log(gameName.charAt(2))
+
+  //  check the position string 
+
+  console.log(gameName.indexOf('e'));
+
+  //  read the string method assignment and use it this programing \
+    //  replace 
+  //  includes check the name 
+  // split method 
+
+
+
+    //  slice and substring
+
+    console.log(gameName.substring(0 , 4))
+
+
+    // slice
+
+    console.log(gameName.slice(-8 , 4))
+
+    // tirm function are used to remove the starrting space and end space 
+   console.log(gameName.trim());
