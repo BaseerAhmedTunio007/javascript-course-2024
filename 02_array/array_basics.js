db5834356eb1ab37a179cff1bf5439d9541c00c2
Baseer Ahmed <102865390+BaseerAhmedTunio007@users.noreply.github.com>
2024-05-04 06:39:52 +0000

    container confige done

---
+++ 02_array/array_basics.js
@@ -0,0 +1,42 @@
+const myArr = [1 , 2 ,3 , 4 ];
+  console.log(myArr[0]);
+   
+  // array method 
+
+  // instances adds the specified elements to the end of an array and returns the new length of the array.
+
+  let num = [1 , 3, 4 ,5];
+   num2 = num.push(8);
+   num2 = num.push(4);
+  //  remove last array 
+   num2 = num.pop(4);
+   console.log(num2);
+
+   const animals = ['pigs', 'goats', 'sheep'];
+   count = animals.unshift('cows'); 
+   count = animals.shift('cows'); 
+  console.log(count);
+
+
+// note shift and unshift
+
+
+// check the position and datatype true are false 
+console.log(num.includes(5));
+console.log(num.indexOf(4))
+
+
+// join are used 
+const newaray=num.join();
+console.log(num);
+console.log(newaray);
+
+
+
+// slice and splice  typic (assignment)
+
+
+
+
+
+
